abf25863ffda89d1cb5cfee6fc2a158a7dcd18b2
LVNAnh <levunhatanh1997@gmail.com>
2025-04-25 20:55:46 +0700

    cap nhat folder api

--- src/api/ApiPath.js
+++ src/api/ApiPath.js
@@ -1,10 +1,17 @@
-import config from '../configs/config';
+import config from "../configs/config";
 
 export const ApiPath = {
   // Auth
-  LOGIN: getApiPath('user/login'),
-  SIGNUP: getApiPath('user/signup'),
-}
+  LOGIN: getApiPath("user/login"),
+  SIGNUP: getApiPath("user/signup"),
+
+  //Venue
+  ADDCOURT: getApiPath("venue/create"),
+  GETALLCOURT: getApiPath("venue/getAll"),
+  GETCOURT: getApiPath("venue/"),
+  UPDATECOURT: getApiPath("venue/"),
+  DELETECOURT: getApiPath("venue/delete/"),
+};
 
 function getApiPath(path) {
   return `${config.app.main_api}/v1/api/${path}`;
